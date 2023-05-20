c331e8e459b68ac7f59d044ae28d2128b2bdcc8c
ujjwal <ujjwalsharma1772003@gmail.com>
2023-05-20 15:58:09 +0530

    changes in signpage

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const https = require("https")
-const request = require("Request");
+// const https = require("https")
+const request = require("request");
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+// app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
-   
+   res.sendFile(__dirname + "/signup.html")
 });
 
 
